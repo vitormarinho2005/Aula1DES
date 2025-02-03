5b65ae3a180cbad7d21a42292c26d613ecf4d503
vitormarinho2005 <vitor_ms_santos@estudante.sesisenai.org.br>
2025-02-03 17:14:10 -0300

    first commit

--- BdB.js
+++ BdB.js
@@ -1,1 +1,15 @@
-console.log("teste")
+function somaNumeros(n) {
+    let soma = 0;
+    for (let i = 1; i <= n; i++) {
+        soma += i;
+    }
+    return soma;
+}
+
+// Exemplo de uso
+const numero = parseInt(prompt("Digite um número: "));
+if (!isNaN(numero) && numero > 0) {
+    console.log(`A soma de 1 até ${numero} é: ${somaNumeros(numero)}`);
+} else {
+    console.log("Por favor, digite um número inteiro positivo.");
+}
